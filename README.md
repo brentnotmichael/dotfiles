72938559a364e0a5072a90252d73086b92309dbe
Brent Michael Gannetta <gannettajr.gmail.com>
2020-05-14 13:53:43 -0700

    updated README.md, deleted old unused files, updated setup-dotfiles.sh to copy files to user's home directory instead of the next directory up

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # dotfiles
 My dotfiles. This is a thing people do, right?
 
+## setup-dotfiles.sh
+Copies directories to current user's home directory as dotfiles (`vim`
+directory as
+`~/.vim` directory)
+Creates hard links from visible files in the dotfiles directory to user's home
+directory as dotfiles (`vimrc` as `~/.vimrc`)
+
 ## .vimrc
 This is my .vimrc
--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -2,8 +2,8 @@
 # Copies all directories as dotfile directories to ~
 # Creates hard links to dotfiles as dotfiles in ~
 
-echo "Copying vim to ../.vim"
-cp -r vim ../.vim
-echo "Creating hard link from vimrc to ../.vimrc"
-ln vimrc ../.vimrc
+echo "Copying vim to home directory ~/.vim"
+cp -r vim ~/.vim
+echo "Creating hard link from vimrc to home directory ~/.vimrc"
+ln vimrc ~/.vimrc
 echo "Done!"
